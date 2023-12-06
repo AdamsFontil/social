11f114912995102ee7e07ed758794c6920d14392
AdamsFontil <108461931+AdamsFontil@users.noreply.github.com>
2023-12-06 10:27:25 -0500

    add new .env file which should be ignored by git

--- app/utils/supabase.ts
+++ app/utils/supabase.ts
@@ -3,3 +3,6 @@ import { createClient, PostgrestResponse } from '@supabase/supabase-js';
 const supabaseUrl = 'https://uhvfikmkgbuykvfmnxzp.supabase.co';
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY!;
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
+
+//fixed this part, it was missing the .env.local file with the appropriate key. keep this noted for next time
