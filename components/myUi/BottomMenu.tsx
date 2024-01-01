609b84796107c799a6451318e03c32ffdc2d485f
AdamsFontil <108461931+AdamsFontil@users.noreply.github.com>
2023-12-31 23:15:02 -0500

    give bg color to bottommenu

--- components/myUi/BottomMenu.tsx
+++ components/myUi/BottomMenu.tsx
@@ -10,7 +10,7 @@ const BottomMenu: React.FC<BottomMenuProps> = ({ type }) => {
   return (
     <div className="flex flex-col justify-between bg-re-500 w-screen fixed bottom-0">
       <div className="flex md:hidden px-8 justify-end bg-yllow-600 py-4"><AddPostMobile type={type} /></div>
-      <div className="flex justify-between px-8 w-full bg-ble-500 py-4 md:hidden border-t">
+      <div className="flex justify-between px-8 w-full bg-background py-4 md:hidden border-t">
       <Link href='/home'><Home /></Link>
       <Link href='/explore'><Search /></Link>
       <Link href='/notifications'><Bell /></Link>
